e1144303825c1a3836e6f9a1f989c0fda1bf6604
Denis Popkov <pkvdenis@gmail.com>
2024-04-03 21:55:32 +0300

    feat: add focus timer [sc-1]

--- src/main/kotlin/ui/App.kt
+++ src/main/kotlin/ui/App.kt
@@ -1,16 +1,27 @@
 package io.dpopkov.apatheiafx.ui
 
 import javafx.application.Application
+import javafx.event.EventHandler
 import javafx.scene.Scene
 import javafx.scene.layout.StackPane
 import javafx.stage.Stage
 
 class App : Application() {
+    private val focusTimer = FocusTimer()
+
     override fun start(primaryStage: Stage) {
         primaryStage.title = "ApatheiaFX"
-        val root = StackPane()
+        primaryStage.onCloseRequest = EventHandler {
+            focusTimer.cancel()
+        }
+
+        val root = StackPane(
+            focusTimer.buildPomodoroNode(),
+        )
         with(primaryStage) {
-            scene = Scene(root, 480.0, 360.0)
+            scene = Scene(root, 480.0, 240.0).apply {
+                stylesheets.add("css/styles.css")
+            }
             show()
         }
     }
---
+++ src/main/kotlin/ui/FocusTimer.kt
@@ -0,0 +1,135 @@
+package io.dpopkov.apatheiafx.ui
+
+import javafx.application.Platform
+import javafx.beans.binding.Bindings
+import javafx.beans.property.SimpleIntegerProperty
+import javafx.scene.Node
+import javafx.scene.control.Alert
+import javafx.scene.control.Button
+import javafx.scene.control.TextField
+import javafx.scene.layout.HBox
+import javafx.scene.layout.VBox
+import javafx.scene.text.Text
+import java.util.*
+
+private const val DEFAULT_SECONDS_INTERVAL = 25 * 60
+
+class FocusTimer {
+    private var focusTimer: Timer? = null
+    private var secondsTimer: Timer? = null
+    private val secondsProperty = SimpleIntegerProperty(DEFAULT_SECONDS_INTERVAL)
+    private val secondsBinding = Bindings.createStringBinding(
+        { secondsProperty.value.formatSecondsToTime() },
+        secondsProperty
+    )
+    private val inputTestingTimeField = TextField().apply {
+        promptText = "Enter custom time interval MM:SS"
+        setOnKeyTyped {
+            if (text.isNotEmpty() && text.isNotBlank()) {
+                secondsProperty.value = text.parseTimeToSeconds()
+            }
+        }
+    }
+
+    fun buildPomodoroNode(): Node {
+        val timerTxt = Text().apply {
+            textProperty().bind(secondsBinding)
+            styleClass.addAll("timer-output-text")
+        }
+        val btnStart = Button("Start")
+        btnStart.setOnAction {
+            println("Clicked Start")
+            println("Scheduling timer for ${secondsProperty.value} seconds")
+            focusTimer = Timer(true).apply {
+                schedule(IntervalTask(), (secondsProperty.value * 1000).toLong())
+            }
+            secondsTimer = Timer(true).apply {
+                scheduleAtFixedRate(SecondsCountdownTask(), 1000L, 1000L)
+            }
+        }
+        val btnStop = Button("Stop")
+        btnStop.setOnAction {
+            println("Clicked Stop")
+            cancelTimers()
+        }
+        val btnReset = Button("Reset")
+        btnReset.setOnAction {
+            println("Clicked Reset")
+            cancelTimers()
+            updateSecondsProperty()
+        }
+
+        return VBox(
+            10.0,
+            inputTestingTimeField,
+            timerTxt,
+            HBox(
+                5.0,
+                btnStart,
+                btnStop,
+                btnReset,
+            ),
+        ).apply {
+            styleClass.addAll("pomodoro-pane")
+        }
+    }
+
+    fun cancel() {
+        cancelTimers()
+    }
+
+    private fun cancelTimers() {
+        secondsTimer?.cancel()
+        focusTimer?.cancel()
+    }
+
+    private fun updateSecondsProperty() {
+        if (inputTestingTimeField.text.isEmpty() || inputTestingTimeField.text.isBlank()) {
+            secondsProperty.value = DEFAULT_SECONDS_INTERVAL
+        } else {
+            secondsProperty.value = inputTestingTimeField.text.parseTimeToSeconds()
+        }
+    }
+
+    private fun Int.formatSecondsToTime(): String {
+        val minutes = (this / 60).toString()
+        val seconds = (this % 60).toString()
+        return "${minutes.padStart(2, '0')}:${seconds.padStart(2, '0')}"
+    }
+
+    private fun String.parseTimeToSeconds(): Int {
+        if (this.contains(':')) {
+            if (this.endsWith(':')) {
+                val minutes = this.trimEnd(':')
+                return minutes.toInt() * 60
+            }
+            val tokens = this.split(':')
+            val minutes = tokens[0]
+            val seconds = tokens[1]
+            return minutes.toInt() * 60 + seconds.toInt()
+        } else {
+            return this.toInt()
+        }
+    }
+
+    private inner class IntervalTask : TimerTask() {
+        override fun run() {
+            Platform.runLater {
+                secondsTimer?.cancel()
+                Alert(Alert.AlertType.INFORMATION).apply {
+                    title = "Focus Interval"
+                    headerText = "Interval is over!"
+                    showAndWait()
+                }
+            }
+        }
+    }
+
+    private inner class SecondsCountdownTask : TimerTask() {
+        override fun run() {
+            Platform.runLater {
+                secondsProperty.value = secondsProperty.value.dec()
+            }
+        }
+    }
+}
