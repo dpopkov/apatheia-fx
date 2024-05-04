be8152363af776cf91730aa1089e28e586ecf5ef
Denis Popkov <pkvdenis@gmail.com>
2024-05-04 23:57:32 +0300

    fix: bug when reading wav from jar [sc-1]

--- src/main/kotlin/ui/AudioPlayer.kt
+++ src/main/kotlin/ui/AudioPlayer.kt
@@ -1,18 +1,35 @@
 package io.dpopkov.apatheiafx.ui
 
 import javafx.scene.media.AudioClip
+import org.slf4j.LoggerFactory
 
 private const val DEFAULT_WAV = "pomodoro-f1.wav"
 
 class AudioPlayer(
     wavResource: String = "sound/$DEFAULT_WAV"
 ) {
+    private val log = LoggerFactory.getLogger(this::class.java)
     private val clip: AudioClip
 
     init {
-        val mediaUrl = this::class.java.classLoader.getResource(wavResource)
+        val mediaUrl = this::class.java.getResource("/$wavResource")
             ?: throw IllegalStateException("Cannot find sound resource: $wavResource")
-        clip = AudioClip(mediaUrl.toExternalForm())
+        val urlStr = mediaUrl.toExternalForm()
+        log.debug("Loading media from path: {}", urlStr)
+        if (urlStr.startsWith("jar:nested:")) {
+            /*
+                Это случай запуска из Jar.
+                Данный Url вызовет ошибку при попытке загрузить его в AudioClip.
+                Поэтому нужно произвести две замены.
+             */
+            val urlStrReplaced = urlStr
+                .replace("jar:nested:", "jar:file:")
+                .replace("jar/!BOOT-INF/classes/!/", "jar!/BOOT-INF/classes/")
+            log.debug("Loading media from modified path: {}", urlStrReplaced)
+            clip = AudioClip(urlStrReplaced)
+        } else {
+            clip = AudioClip(urlStr)
+        }
     }
 
     fun play() {
